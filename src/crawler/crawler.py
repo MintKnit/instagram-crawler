cd12aa2ca0b2cbbd97af47e983934c3e81812986
donghunkim <ddong3525@gmail.com>
2023-01-31 01:00:49 +0900

    Second commit

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,49 +1,91 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import requests
-
-username = "ampersandOr_"
-password = input() # 땅콩
-
-# create a new Firefox browser instance
-driver = webdriver.Chrome(executable_path='../../lib/chromedriver')
-
-driver.implicitly_wait(5)
-
-# navigate to Instagram's login page
-driver.get("https://www.instagram.com/accounts/login/")
-
-# find the username and password input fields and enter your credentials
-username_input = driver.find_element(By.NAME, 'username')
-password_input = driver.find_element(By.NAME, "password")
-username_input.send_keys(username)
-password_input.send_keys(password)
+from time import sleep
 
 
-# find the login button and click it
-login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-login_button.click()
+class Crawler:
+    def _generate_chrome_option(self):
+         # Create a ChromeOptions object
+        options = webdriver.ChromeOptions()
+        # Add the option to preserve cookies
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-browser-side-navigation")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-infobars")
+        options.add_argument("start-maximized")
+        options.add_argument("disable-popup-blocking")
+        options.add_argument("--disable-default-apps")
+        options.add_argument("--enable-automation")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-notifications")
+        options.add_argument("--disable-single-click-autofill")
+        options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
+        options.add_argument("--disable-background-networking")
+        options.add_argument("--disable-background-timer-throttling")
+        options.add_argument("--disable-backgrounding-occluded-windows")
+        options.add_argument("--disable-breakpad")
+        options.add_argument("--disable-client-side-phishing-detection")
+        options.add_argument("--disable-default-plugins")
+        options.add_argument("--disable-hang-monitor")
+        options.add_argument("--disable-ipc-flooding-protection")
+        options.add_argument("--disable-popup-blocking")
+        options.add_argument("--disable-prompt-on-repost")
+        options.add_argument("--disable-sync")
+        options.add_argument("--force-color-profile=srgb")
+        options.add_argument("--metrics-recording-only")
+        options.add_argument("--password-store=basic")
+        options.add_argument("--use-mock-keychain")
+        options.add_argument("--disable-translate")
+        options.add_argument("--disable-web-security")
+        return options
+
+    def __init__(self, webdriver_path):
+        # Initialize the webdriver with the options
+        self.driver = webdriver.Chrome(executable_path=webdriver_path, options=self._generate_chrome_option())
+        self.driver.implicitly_wait(5)
+    
+    def login(self, login_page, username, password):
+        # navigate to Instagram's login page
+        self.driver.get(login_page)
+        username_input = self.driver.find_element(By.NAME, "username")
+        password_input = self.driver.find_element(By.NAME, "password")
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        # find the login button and click it
+        login_button = self.driver.find_element(By.XPATH, "//button[@type='submit']")
+        login_button.click()
 
 
-driver.get("https://www.instagram.com/explore/tags/맛집/")
+    def scrap(self, hashtag_name):
+        self.driver.get(f"https://www.instagram.com/explore/tags/{hashtag_name}/")
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        article = self.driver.find_element(By.XPATH, "//article")
+        elements = article.find_elements(By.XPATH, "//a[@role='link']")
+        links = [element.get_attribute("href") for element in elements if element.get_attribute("href").startswith("https://www.instagram.com/p/")]
 
-import time
-time.sleep(10)
+        print(len(links))
+        for link in links:     
+            self.driver.get(link)
+            time = self.driver.find_element(By.XPATH, "//time")
+            print(link, time.get_attribute("datetime"))
+            sleep(3)
 
+if __name__ == "__main__":
+    webdriver_path = '../../lib/chromedriver'
+    crawler = Crawler(webdriver_path)
 
+    # username = "ampersandOr_"
+    # password = input()
+    # login_page = "https://www.instagram.com/accounts/login/"
+    # crawler.login(login_page, username, password)
+    # print("wait for the login...(3s)")
+    # sleep(3)
+    print("go scrapping")
+    hashtag_names = ["햄버거", "피자",  "김밥", "국밥", "초밥", "삼겹살", "소고기", "곱창", "회"]
+    for hashtag_name in hashtag_names:
+        crawler.scrap(hashtag_name)
+        print("done")
 
-# navigate to a user's profile
-driver.get("https://www.instagram.com/instagram/")
-
-# find the number of posts on the user's profile
-post_count = driver.find_element_by_xpath("//span[@class='g47SY ']")
-print(f"{post_count.text} posts")
-
-# find the number of followers on the user's profile
-follower_count = driver.find_element_by_xpath("//a[@href='/instagram/followers/']/span")
-print(f"{follower_count.text} followers")
-
-# Close the browser
-driver.close()
 
