37f9ab0b229c6758642060d5e37e6c3ee2231773
dhkim4 <dhkim4@seegene.com>
2023-02-14 11:22:13 +0900

    added food category to a kafka topic

--- src/fake_crawler/fake_crawler.py
+++ src/fake_crawler/fake_crawler.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 from kafka import KafkaProducer
 import time
+import random
 
 _BOOTSTRAP_IP = "10.12.168.74"
 _BOOTSTRAP_PORT = "9092"
+
+FOOD_LIST = ["burger", "pizza", "kookbab", "kimbab", "pasta"]
 
 def main():
 
@@ -14,9 +17,13 @@ def main():
     topic = "food"
 
     while True:
-        now = str(datetime.now())
-        producer.send(topic, now.encode())
-        print(now)
+        food = random.choice(FOOD_LIST)
+        data = {
+            'timestamp': str(datetime.now()),
+            'category': category
+        }
+        message = json.dumps(data).encode()
+        producer.send(topic, message)
         time.sleep(10)
 
 if __name__ == "__main__":
